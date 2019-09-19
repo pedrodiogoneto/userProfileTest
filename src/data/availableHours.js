83ffadf2a96a125eabf163406080d1dd1cac1bf5
pedrodiogoneto <pedritoneto@gmail.com>
2019-09-19 17:11:06 +0100

    Add mocked Data

---
+++ src/data/availableHours.js
@@ -0,0 +1,39 @@
+const availableHours = {
+    1566172800: {
+        '08:00': false,
+        '08:30': false,
+        '09:00': false,
+        '09:30': false,
+        '10:00': false,
+        '10:30': false,
+        '11:00': false
+    },
+    1566259200: {
+        '08:00': true,
+        '08:30': true,
+        '09:00': true,
+        '09:30': true,
+        '10:00': true,
+        '10:30': true,
+        '11:00': true
+    },
+    1566345600: {
+        '08:00': true,
+        '08:30': true,
+        '09:00': true,
+        '09:30': true,
+        '10:00': true,
+        '10:30': true,
+        '11:00': true
+    },
+    1566432000: {
+        '08:00': true,
+        '08:30': true,
+        '09:00': true,
+        '09:30': true,
+        '10:00': true,
+        '10:30': true,
+        '11:00': true    }
+}
+
+export default availableHours
---
+++ src/data/selectedUser.js
@@ -0,0 +1,13 @@
+const selectedUser = {
+    name: "Professional name",
+    category: "PSICOLOGIST",
+    location: "Lisbon",
+    rating: [1,1,1,1,1],
+    reviewsNumber: "20",
+    rateValue: "R$160",
+    rateTime: "50 MINUTES",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    pictureSrc: 'https://avatars.servers.getgo.com/2205256774854474505_medium.jpg'
+}
+
+export default selectedUser
